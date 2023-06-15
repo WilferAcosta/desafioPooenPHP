dc71a27911027ba34a0a7ec30afa0180d2dbc60d
WilferAcosta <w.o.a.r@hotmail.es>
2023-06-15 14:33:58 -0500

    feat: :rocket: finalizado
    termine el ejercicio propuesto de class abstracta

--- app.php
+++ app.php
@@ -1,21 +1,48 @@
 <?php
 
-abstract class CuentaBancaria{
-    public function __construct(protected int $saldo){
-        $this->saldo = $saldo;
+abstract class CuentaBancaria {
+    public function __construct(public int $saldo) {
     }
-    
-    public function depositar($monto){
-        return $this->saldo+=$monto;
+
+    public function depositar($monto) {
+        return $this->saldo += $monto;
     }
-    public function retirar($monto){
-        if($this->saldo >= $monto){
-            $this->saldo-=$monto;
-            return "retiro exitoso";
-        }else{
+
+    public function retirar($monto) {
+        if ($this->saldo >= $monto) {
+            $this->saldo -= $monto;
+            return "Retiro exitoso";
+        } else {
             return "El monto a retirar es superior a su saldo.";
         }
     }
 }
+
+class CuentaCorriente extends CuentaBancaria {
+    public function retirar($monto) {
+        if ($this->saldo >= $monto) {
+            $this->saldo -= $monto;
+            return "Retiro exitoso";
+        } else {
+            $sobregiro = $monto - $this->saldo;
+            $cargo_sobregiro = $sobregiro * 0.1;
+            $this->saldo = 0;
+            return "Saldo insuficiente. Se ha aplicado un cargo por sobregiro de: " . $cargo_sobregiro;
+        }
+    }
+}
+
+class CuentaAhorros extends CuentaBancaria {
+    // No es necesario sobrescribir el método retirar, se hereda de CuentaBancaria
+}
+
+$cuentaCorriente = new CuentaCorriente(100);
+echo $cuentaCorriente->retirar(50) . "<br>"; // Retiro exitoso
+echo $cuentaCorriente->retirar(200) . "<br>"; // Saldo insuficiente. Se ha aplicado un cargo por sobregiro de: 10
+
+$cuentaAhorros = new CuentaAhorros(200);
+echo $cuentaAhorros->retirar(150) . "<br>"; // Retiro exitoso
+echo $cuentaAhorros->retirar(300) . "<br>"; // El monto a retirar es superior a su saldo.
+
 
 ?>
